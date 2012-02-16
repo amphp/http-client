26a37c6e12a3f7404df0e8ea32c03a94fa92d1f5
Daniel Lowrey <rdlowrey@gmail.com>
2012-02-16 13:29:21 -0500

    Updated HandlersInterface code documentation

--- src/artax/HandlersInterface.php
+++ src/artax/HandlersInterface.php
@@ -36,10 +36,19 @@ namespace artax {
      */
     public function shutdown();
     
+    /**
+     * Outputs response to client when the requested route is not found
+     */
     public function notFound();
     
+    /**
+     * Outputs response to client when an unexpected internal error is encountered
+     */
     public function unexpectedError(\Exception $e);
     
+    /**
+     * Specify if full debug message should be shown for unexpected exceptions
+     */
     public function setDebug($val);
   }
 }
