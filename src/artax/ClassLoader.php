defb807b36782498855f15aa456e6911b51f1cc9
Daniel Lowrey <rdlowrey@gmail.com>
2012-02-16 12:04:45 -0500

    Modified SplClassLoader to allow inheritance and moved its functionality into core ClassLoader

--- src/artax/ClassLoader.php
+++ src/artax/ClassLoader.php
@@ -3,16 +3,44 @@
 /**
  * Artax ClassLoader
  * 
- * Class is based heavily on the original SplClassLoader specified by the PSR-0
- * standard found here:
+ * Class is based very heavily on the original SplClassLoader specified by the
+ * PSR-0 standard example found here:
  * 
  * http://groups.google.com/group/php-standards/web/final-proposal
  * 
+ * The only differences in this iteration are:
+ *  - Aesthetic code formatting changes
+ *  - Variable name changes
+ *  - Class properties that used to be private are now protected to allow
+ * subclassing for cache support
  * 
+ * @category artax
+ * @package  core
+ * @author   Jonathan H. Wage <jonwage@gmail.com>
+ * @author   Roman S. Borschel <roman@code-factory.org>
+ * @author   Matthew Weier O'Phinney <matthew@zend.com>
+ * @author   Kris Wallsmith <kris.wallsmith@gmail.com>
+ * @author   Fabien Potencier <fabien.potencier@symfony-project.org>
+ * @author   Daniel Lowrey <rdlowrey@gmail.com>
  */
 
 namespace artax {
-
+  
+  /**
+   * Allows PSR-0 compliant registration of namespaced class autoloading
+   * 
+   * The class is essentially the same as the original specification here:
+   * http://groups.google.com/group/php-standards/web/final-proposal
+   * 
+   * @category artax
+   * @package  core
+   * @author   Jonathan H. Wage <jonwage@gmail.com>
+   * @author   Roman S. Borschel <roman@code-factory.org>
+   * @author   Matthew Weier O'Phinney <matthew@zend.com>
+   * @author   Kris Wallsmith <kris.wallsmith@gmail.com>
+   * @author   Fabien Potencier <fabien.potencier@symfony-project.org>
+   * @author   Daniel Lowrey <rdlowrey@gmail.com>
+   */
   class ClassLoader
   {
     protected $ext = '.php';
@@ -124,9 +152,9 @@ namespace artax {
         $fileName = '';
         $namespace = '';
         if (false !== ($lastNsPos = strripos($className, $this->nsSeparator))) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName = str_replace($this->nsSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+          $namespace = substr($className, 0, $lastNsPos);
+          $className = substr($className, $lastNsPos + 1);
+          $fileName = str_replace($this->nsSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
         }
         $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->ext;
         require ($this->includePath !== null ? $this->includePath . DIRECTORY_SEPARATOR : '') . $fileName;
