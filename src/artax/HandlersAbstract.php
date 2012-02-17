6f1e3c53df23afff1a6a33c55c97cee1a06e331a
Daniel Lowrey <rdlowrey@gmail.com>
2012-02-17 14:37:11 -0500

    Added HandlersAbstract unit tests and cleaned up code

--- src/artax/HandlersAbstract.php
+++ src/artax/HandlersAbstract.php
@@ -47,11 +47,11 @@ namespace artax {
      * Handle unexpected fatal errors
      *
      * @return void
-     * @uses HandlersAbstract::fatalErrorOccurred
+     * @uses HandlersAbstract::getFatalErrException
      */
     public function shutdown()
     {
-      if ($e = $this->fatalErrorOccurred()) {
+      if ($e = $this->getFatalErrException()) {
         $this->unexpectedError($e);
       }
     }
@@ -60,37 +60,44 @@ namespace artax {
      * Determine if the last triggered PHP error was fatal
      * 
      * If the last occuring error during script execution was fatal the function
-     * returns a `artax\exceptions\RuntimeException` object representing the error so it
-     * can be handled by the shutdown handler.
+     * returns a `artax\exceptions\RuntimeException` object representing the error
+     * so it can be handled by the unexpectedError handler.
      * 
-     * @return mixed bool(FALSE) if none/non-fatal error -or- RuntimeException if
+     * @return mixed Returns NULL if none/non-fatal error -or- RuntimeException if
      *               last error occurence was fatal.
      */
-    protected function fatalErrorOccurred()
+    public function getFatalErrException()
     {
-      if ( ! $err = error_get_last()) {
-        return FALSE;
-      }
-      $str = "Uncaught exception 'artax\exceptions\ScriptHaltException'";
-      if (strstr($err['message'], $str)) {
-        return FALSE;
-      }
-
-      $fatals = array(
-        E_ERROR           => 'Fatal Error',
-        E_PARSE           => 'Parse Error',
-        E_CORE_ERROR      => 'Core Error',
-        E_CORE_WARNING    => 'Core Warning',
-        E_COMPILE_ERROR   => 'Compile Error',
-        E_COMPILE_WARNING => 'Compile Warning'
-      );
+      $ex  = NULL;
+      $err = $this->lastError();
       
-      if (isset($fatals[$err['type']])) {
-        $msg = $fatals[$err['type']] . ': ' . $err['message'] . ' in ';
-        $msg.= $err['file'] . ' on line ' . $err['line'];
-        return new exceptions\RuntimeException($msg);
+      if (NULL !== $err && ! stristr($err['message'], 'ScriptHaltException')) {
+        $fatals = [
+          E_ERROR           => 'Fatal Error',
+          E_PARSE           => 'Parse Error',
+          E_CORE_ERROR      => 'Core Error',
+          E_CORE_WARNING    => 'Core Warning',
+          E_COMPILE_ERROR   => 'Compile Error',
+          E_COMPILE_WARNING => 'Compile Warning'
+        ];
+        
+        if (isset($fatals[$err['type']])) {
+          $msg = $fatals[$err['type']] . ': ' . $err['message'] . ' in ';
+          $msg.= $err['file'] . ' on line ' . $err['line'];
+          $ex = new exceptions\RuntimeException($msg);
+        }
       }
-      return FALSE;
+      return $ex;
+    }
+    
+    /**
+     * Get an array representation of the most recently raised PHP error
+     * 
+     * @return array Returns an associative error representation array
+     */
+    protected function lastError()
+    {
+      return error_get_last();
     }
     
     /**
--- test/src/artax/HandlersAbstractTest.php
+++ test/src/artax/HandlersAbstractTest.php
@@ -42,8 +42,65 @@ class HandlersAbstractTest extends PHPUnit_Framework_TestCase
     $ha->expects($this->once())->method('unexpectedError');
     $ha->exHandler(new Exception);
   }
+  
+  /**
+   * @covers artax\HandlersAbstract::shutdown
+   */
+  public function testShutdownCallsUnexpectedErrorAsExpected()
+  {
+    $ha = $this->getMock('HandlersAbstractTestClass', ['getFatalErrException']);
+    $ha->expects($this->any())
+               ->method('getFatalErrException')
+               ->will($this->returnValue(FALSE));
+    $ha->shutdown();
+    
+    $ha = $this->getMock('HandlersAbstractTestClass', ['getFatalErrException']);
+    $ha->expects($this->any())
+               ->method('getFatalErrException')
+               ->will($this->returnValue(new \Exception));
+    $ha->shutdown();
+  }
+  
+  /**
+   * artax\HandlersAbstract::getFatalErrException
+   */
+  public function testGetFatalErrExceptionReturnsExOnFatalError()
+  {
+    $lastErr = [
+      'type'    => 1,
+      'message' => 'Fatal Error: uscSucks',
+      'file'    => '/my/sweet/script.php',
+      'line'    => 42
+    ];
+    $ha = $this->getMock('HandlersAbstractTestClass', ['lastError']);
+    $ha->expects($this->any())
+       ->method('lastError')
+       ->will($this->returnValue($lastErr));
+    
+    $this->assertTrue($ha->getFatalErrException() 
+      instanceof artax\exceptions\RuntimeException);
+  }
+  
+  /**
+   * artax\HandlersAbstract::getFatalErrException
+   * artax\HandlersAbstract::lastError
+   */
+  public function testGetFatalErrExceptionReturnsNullOnNonFatalError()
+  {
+    $handlers = new HandlersAbstractTestClass;
+    $this->assertNull($handlers->getFatalErrException());
+  }
 }
 
+class HandlersAbstractTestClass extends artax\HandlersAbstract
+{
+  public function notFound()
+  {
+  }
+  public function unexpectedError(\Exception $e)
+  {
+  }
+}
 
 
 
