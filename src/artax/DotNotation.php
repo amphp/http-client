2a45064a26f5bb7229d35200d77491d93421b6a2
Daniel Lowrey <rdlowrey@gmail.com>
2012-02-24 12:09:19 -0500

    Added reverse DotNotation parsing capability

--- src/artax/DotNotation.php
+++ src/artax/DotNotation.php
@@ -26,28 +26,29 @@ namespace artax {
   class DotNotation
   {
     /**
-     * Parses namespaced class and method components from dot notation string
+     * Parses namespaced class and method components to and from dot notation
      * 
-     * @param string $dotStr The dot-notation string to parse
-     * @param bool   $method Whether or not the string contains a method
+     * @param string $dotStr  The dot-notation string to parse
+     * @param bool   $reverse Parse a standard class name to dot notation
+     * @param bool   $method  Whether or not the string contains a method
      * 
      * @return mixed Returns an array of class and method name or a string class
      *               name if the `$method` parameter is FALSE.
      */
-    public function parse($dotStr, $method=FALSE)
+    public function parse($dotStr, $reverse=FALSE, $method=FALSE)
     {
-      $str = trim($dotStr, '.');
+      $repl = ! $reverse ? '.' : '\\';
+      $new  = ! $reverse ? '\\' : '.';
+      $cls  = ! $reverse ? '\\' : ''; // Don't prepend dot to class names on reverse
+      $str  = trim($dotStr, $repl);
       
-      if ( ! $str) {
-        $msg = 'Invalid dot notation string: ' . $dotStr ?: 'none';
-        throw new exceptions\InvalidArgumentException($msg);
-      } elseif ( ! $method) {
-        $r = '\\' . str_replace('.', '\\', $str);
-      } elseif (strstr($str, '.')) {
-        $parts = explode('.', $str);
+      if ( ! $method) {
+        $r = $cls . str_replace($repl, $new, $str);
+      } elseif (strstr($str, $repl)) {
+        $parts = explode($repl, $str);
         $func  = array_pop($parts);
-        $class  = '\\' . implode('\\', $parts);
-        $r = [$class, $func];
+        $cls  .= implode($new, $parts);
+        $r     = [$cls, $func];
       }
       return $r;
     }
--- test/src/artax/DotNotationTest.php
+++ test/src/artax/DotNotationTest.php
@@ -9,7 +9,7 @@ class DotNotationTest extends PHPUnit_Framework_TestCase
   {
     $dn = new artax\DotNotation;
     $dotStr = 'namespace.ClassName.methodName';
-    $arr = $dn->parse($dotStr, TRUE);
+    $arr = $dn->parse($dotStr, FALSE, TRUE);
     
     $this->assertEquals('\namespace\ClassName', $arr[0]);
     $this->assertEquals('methodName', $arr[1]);
@@ -22,28 +22,9 @@ class DotNotationTest extends PHPUnit_Framework_TestCase
   {
     $dn = new artax\DotNotation;
     $dotStr = 'namespace.ClassName.methodName';
-    $str = $dn->parse($dotStr, FALSE);
+    $str = $dn->parse($dotStr);
     
     $this->assertEquals('\namespace\ClassName\methodName', $str);
-  }
-  
-  /**
-   * @covers artax\DotNotation::parse
-   */
-  public function testParseThrowsExceptionOnInvalidString()
-  {
-    $invalid = [NULL, '...', '.'];
-    $dn = new artax\DotNotation;
-    foreach ($invalid as $dotStr) {
-      try {
-        $ex = FALSE;
-        $dn->parse($dotStr);
-      } catch (artax\exceptions\InvalidArgumentException $e) {
-        $ex = TRUE;
-      }
-      $msg = "Failed to throw exception on invalid dot notation string: $dotStr";
-      $this->assertTrue($ex, $msg);
-    }
   }
 }
 
