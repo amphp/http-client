560af7f2b219b7a4f6e1e532fc346b3fb92bd46f
Daniel Lowrey <rdlowrey@gmail.com>
2012-03-01 15:39:58 -0500

    [ConfigTest] Updated unit test

--- test/src/artax/ConfigTest.php
+++ test/src/artax/ConfigTest.php
@@ -12,7 +12,7 @@ class ConfigTest extends PHPUnit_Framework_TestCase
    */
   public function testConstructorInitializesParamDefaults()
   {
-    $c = new artax\Config;
+    $c = new ConfigTestCoverageImplementation;
     $this->assertEquals(FALSE, $c->get('debug'));
   }
   
@@ -24,8 +24,12 @@ class ConfigTest extends PHPUnit_Framework_TestCase
   public function testFilterBoolSanitizesBoolInput()
   {
     $params = ['debug'=>0, 'httpBundle'=>'Off', 'cliBundle'=> 'on'];
-    $c = new artax\Config($params);
+    $c = new ConfigTestCoverageImplementation($params);
     $this->assertEquals(FALSE, $c->get('debug'));
     $this->assertEquals(FALSE, $c->get('httpBundle'));
   }
+}
+
+class ConfigTestCoverageImplementation extends artax\Config
+{
 }
