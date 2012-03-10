6dd8e7c5cf7f44717105ed1fb8a11e78c1b80cd3
Daniel Lowrey <rdlowrey@gmail.com>
2012-03-09 22:31:22 -0500

    + unit-tests for Bootstrapper class

---
+++ test/src/Artax/BootstrapperTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class BootstrapperTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Artax\Bootstrapper::__construct
+     */
+    public function testConstructorAssignsProperties()
+    {
+        $med  = new Artax\Events\Mediator;
+        $term = new BootstrapperTerminationHandlerTestClass;
+        $boot = new BootstrapperTestImplementationClass($term, $med);
+        
+        $this->assertEquals($med, $boot->mediator);
+        $this->assertEquals($term, $boot->termination);
+        
+        return $boot;
+    }
+    
+    /**
+     * @depends testConstructorAssignsProperties
+     * @covers  Artax\Bootstrapper::boot
+     */
+    public function testBootInjectsMediatorIntoTermHandlerAndReturnsMediator($boot)
+    {
+        $this->assertEquals($boot->mediator, $boot->boot());
+        $this->assertEquals($term->mediator, $boot->mediator);
+    }
+      
+}
+
+class BootstrapperTestImplementationClass extends Artax\Bootstrapper
+{
+    use MagicTestGetTrait;
+}
+
+class BootstrapperTerminationHandlerTestClass extends Artax\Handlers\Termination
+{
+    use MagicTestGetTrait;
+}
