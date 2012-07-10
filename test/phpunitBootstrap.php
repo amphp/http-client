872d278240a7944f50d0a17d5a08b21688b31760
Daniel Lowrey <rdlowrey@gmail.com>
2012-07-09 20:12:48 -0400

    Updated phpunit initialization scripts

---
+++ test/phpunitBootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+define('ARTAX_SYSDIR', dirname(__DIR__));
+
+spl_autoload_register(function($cls) {
+    if (0 === strpos($cls, 'Artax\\')) {
+        $cls = str_replace('\\', '/', $cls);        
+        require ARTAX_SYSDIR . "/src/$cls.php";
+    }
+});
+
