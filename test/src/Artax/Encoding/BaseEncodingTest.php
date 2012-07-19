a3d2bb2373fbb61d297683898cdcb484b02b6c94
Daniel Lowrey <rdlowrey@gmail.com>
2012-07-19 14:36:56 -0400

    updated BaseEncodingTest to (hopefully) fix zlib errors

--- test/src/Artax/Encoding/BaseEncodingTest.php
+++ test/src/Artax/Encoding/BaseEncodingTest.php
@@ -4,7 +4,13 @@ abstract class BaseEncodingTest extends PHPUnit_Framework_TestCase {
     
     public function setUp() {
         if (!extension_loaded('zlib')) {
-            $this->markTestSkipped('zlib extension required for Encoding packag tests');
+            $this->markTestSkipped('zlib extension required for Encoding package tests');
+        } elseif (!function_exists('gzdecode')
+            || !function_exists('gzencode')
+            || !function_exists('gzinflate')
+            || !function_exists('gzdeflate')
+        ) {
+            $this->markTestSkipped('zlib extension required for Encoding package tests');
         }
     }
     
