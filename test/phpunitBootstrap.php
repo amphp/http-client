fe8b22dd2836ee7eb64c727b3006ac38e02d8fa5
Daniel Lowrey <rdlowrey@gmail.com>
2012-07-18 23:49:25 -0400

    removed constant from testing bootstrap for better multi-process testing

--- test/phpunitBootstrap.php
+++ test/phpunitBootstrap.php
@@ -1,11 +1,9 @@
 <?php
 
-define('ARTAX_SYSDIR', dirname(__DIR__));
-
 spl_autoload_register(function($cls) {
     if (0 === strpos($cls, 'Artax\\')) {
         $cls = str_replace('\\', '/', $cls);        
-        require ARTAX_SYSDIR . "/src/$cls.php";
+        require dirname(__DIR__) . "/src/$cls.php";
     }
 });
 
