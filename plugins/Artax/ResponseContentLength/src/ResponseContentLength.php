d05822802ca41a0da8302cc2c8611577831d7e3e
Daniel Lowrey <rdlowrey@gmail.com>
2012-08-02 00:11:20 -0400

    Content-Length only added if response body exists and no Transfer-Encoding applied

--- plugins/Artax/ResponseContentLength/src/ResponseContentLength.php
+++ plugins/Artax/ResponseContentLength/src/ResponseContentLength.php
@@ -32,7 +32,18 @@ class ResponseContentLength {
      * @return void
      */
     public function setContentLengthHeader(Response $response) {
-        $contentLength = strlen($response->getBody());
-        $response->setHeader('Content-Length', $contentLength);
+        // http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.3
+        // 
+        // "The presence of a message-body in a request is signaled by the inclusion of a
+        // Content-Length  or Transfer-Encoding header field in the request's message-headers."
+        //
+        // http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.4
+        // 
+        // "If a message is received with both a Transfer-Encoding header field and a Content-Length
+        // header field, the latter MUST be ignored."
+        if (!$response->hasHeader('TransferEncoding') && $body = $response->getBody()) {
+            $contentLength = strlen($body);
+            $response->setHeader('Content-Length', $contentLength);
+        }
     }
 }
