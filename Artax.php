1aed5901226bb34e1ccb0bb26edccf588a8461a3
David <alusitanian@gmail.com>
2012-08-03 00:14:00 -0300

    Added mbstring.func_overload check

--- Artax.php
+++ Artax.php
@@ -17,6 +17,12 @@ if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50300) {
     die('Artax requires PHP 5.3 or higher' . PHP_EOL);
 }
 
+if (extension_loaded('mbstring')) { 
+    if( ini_get('mbstring.func_overload') & 2 ) { 
+        throw new \RuntimeException('Artax requires that string functions not be overloaded by "mbstring.func_overload"');
+    }
+}
+
 spl_autoload_register(function($className) {
     if (0 === strpos($className, 'Artax\\')) {
         $className = str_replace('\\', '/', $className);      
