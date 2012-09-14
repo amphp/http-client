c8647e7dc72b0004d6823ec1d06f04845d8ed72f
Levi Morrison <levim@php.net>
2012-09-14 10:59:59 -0600

    Re-added the '\' to '/' conversion.  Needed on my system.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,6 +3,7 @@
 // Register Artax autoloader
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Artax\\')) {   
+        $class = str_replace('\\', '/', $class);
         require dirname(__DIR__) . "/src/$class.php";
     }
 });
@@ -10,6 +11,7 @@ spl_autoload_register(function($class) {
 // Register PHP-Datastructures autoloader
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Spl\\')) {
+        $class = str_replace('\\', '/', $class);
         $file = dirname(__DIR__) . "/vendor/PHP-Datastructures/src/$class.php";
         require $file;
     }
