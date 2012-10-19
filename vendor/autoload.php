82bbb47006bb3ece438fd2fdaebee9e33a812f0b
Daniel Lowrey <rdlowrey@gmail.com>
2012-10-19 13:57:39 -0400

    Artax autoload function now plays nice

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -3,7 +3,10 @@
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Artax\\')) {
         $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-        require __DIR__ . "/../src/$class.php";
+        $file = __DIR__ . "/../src/$class.php";
+        if (file_exists($file)) {
+            require $file;
+        }
     }
 });
 
