5133d01b8a7e04bccfaced6668e03fb777876838
Daniel Lowrey <rdlowrey@gmail.com>
2012-10-22 16:21:02 -0400

    Killed `../` in favor of `dirname()` in autoloader

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -3,7 +3,7 @@
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Artax\\')) {
         $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-        $file = __DIR__ . "/../src/$class.php";
+        $file = dirname(__DIR__) . "/src/$class.php";
         if (file_exists($file)) {
             require $file;
         }
