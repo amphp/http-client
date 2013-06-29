cf8d731becc9e2fbcee3aca2b26c68660527b6ce
Daniel Lowrey <rdlowrey@gmail.com>
2013-06-29 17:59:22 -0400

    ProgressDisplay::display() is a utility function and as such should be static

--- src/Artax/Ext/Progress/ProgressDisplay.php
+++ src/Artax/Ext/Progress/ProgressDisplay.php
@@ -4,13 +4,12 @@ namespace Artax\Ext\Progress;
 
 class ProgressDisplay {
     
-    function display(ProgressState $ps) {
+    static function display(ProgressState $ps) {
         $kbRcvd = number_format(round($ps->bytesRcvd / 1024));
         $kbps = number_format(round($ps->bytesPerSecond / 1024));
         
-        $output = "\r"; // Clears the previous iteration of the progress bar from the console
-        $output.= $ps->progressBar;
-        $output.= ' ' . $kbRcvd . ' KB';
+        $output = $ps->progressBar . ' ' . $kbRcvd . ' KB';
+        
         if (isset($ps->percentComplete)) {
             $output.= ' (' . round($ps->percentComplete * 100) . '%)';
         }
