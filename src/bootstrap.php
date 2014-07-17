fa3edd3bc6dacb480ec5d44119964b13a7692976
Daniel Lowrey <rdlowrey@php.net>
2014-07-17 10:39:08 -0400

    Add src dir

---
+++ src/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function($class) {
+    if (strpos($class, 'Artax\\') === 0) {
+        $name = substr($class, strlen('Artax'));
+        require __DIR__ . "/../lib" . strtr($name, '\\', DIRECTORY_SEPARATOR) . '.php';
+    }
+});
+
+require __DIR__ . '../vendor/Alert/src/bootstrap.php';
+require __DIR__ . '../vendor/After/src/bootstrap.php';
+require __DIR__ . '../vendor/Addr/src/bootstrap.php';
