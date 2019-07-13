ac30d43dbb0476730f6366bca84ac8adf85f4905
Aaron Piotrowski <aaron@trowski.com>
2019-07-13 10:36:42 -0500

    Move RequestWriter into Http1Connection

--- src/Connection/Http1Connection.php
+++ src/Connection/Http1Connection.php
@@ -12,7 +12,6 @@ use Amp\Emitter;
 use Amp\Http;
 use Amp\Http\Client\HttpException;
 use Amp\Http\Client\Internal\CombinedCancellationToken;
-use Amp\Http\Client\Internal\RequestWriter;
 use Amp\Http\Client\Internal\ResponseBodyStream;
 use Amp\Http\Client\ParseException;
 use Amp\Http\Client\Request;
@@ -20,6 +19,8 @@ use Amp\Http\Client\RequestBody;
 use Amp\Http\Client\Response;
 use Amp\Http\Client\SocketException;
 use Amp\Http\Client\TimeoutException;
+use Amp\Http\InvalidHeaderException;
+use Amp\Http\Rfc7230;
 use Amp\Loop;
 use Amp\NullCancellationToken;
 use Amp\Promise;
@@ -68,7 +69,7 @@ final class Http1Connection implements Connection
 
     public function isBusy(): bool
     {
-        return $this->busy;
+        return $this->busy || $this->socket->isClosed();
     }
 
     public function onClose(callable $onClose): void
@@ -147,13 +148,10 @@ final class Http1Connection implements Connection
             });
 
             try {
-                yield RequestWriter::writeRequest($this->socket, $request, $protocolVersion);
-
+                yield from $this->writeRequest($request, $protocolVersion);
                 return yield from $this->doRead($request, $cancellation, $readingCancellation, $completionDeferred);
-            } catch (HttpException $e) {
+            } finally {
                 $cancellation->throwIfRequested();
-
-                throw $e;
             }
         });
     }
@@ -416,5 +414,86 @@ final class Http1Connection implements Connection
         }
 
         throw new HttpException("None of the requested protocol versions is supported: " . \implode(", ", $protocolVersions));
+    }
+
+    private function writeRequest(Request $request, string $protocolVersion): \Generator
+    {
+        try {
+            $rawHeaders = $this->generateRawHeader($request, $protocolVersion);
+            yield $this->socket->write($rawHeaders);
+
+            $body = $request->getBody()->createBodyStream();
+            $chunking = $request->getHeader("transfer-encoding") === "chunked";
+            $remainingBytes = $request->getHeader("content-length");
+
+            if ($chunking && $protocolVersion === "1.0") {
+                throw new HttpException("Can't send chunked bodies over HTTP/1.0");
+            }
+
+            // We always buffer the last chunk to make sure we don't write $contentLength bytes if the body is too long.
+            $buffer = "";
+
+            while (null !== $chunk = yield $body->read()) {
+                if ($chunk === "") {
+                    continue;
+                }
+
+                if ($chunking) {
+                    $chunk = \dechex(\strlen($chunk)) . "\r\n" . $chunk . "\r\n";
+                } elseif ($remainingBytes !== null) {
+                    $remainingBytes -= \strlen($chunk);
+
+                    if ($remainingBytes < 0) {
+                        throw new HttpException("Body contained more bytes than specified in Content-Length, aborting request");
+                    }
+                }
+
+                yield $this->socket->write($buffer);
+                $buffer = $chunk;
+            }
+
+            // Flush last buffered chunk.
+            yield $this->socket->write($buffer);
+
+            if ($chunking) {
+                yield $this->socket->write("0\r\n\r\n");
+            } elseif ($remainingBytes !== null && $remainingBytes > 0) {
+                throw new HttpException("Body contained fewer bytes than specified in Content-Length, aborting request");
+            }
+        } catch (StreamException $exception) {
+            throw new SocketException('Socket disconnected prior to response completion');
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @param string  $protocolVersion
+     *
+     * @return string
+     *
+     * @throws HttpException
+     */
+    private function generateRawHeader(Request $request, string $protocolVersion): string
+    {
+        // TODO: Send absolute URIs in the request line when using a proxy server
+        //  Right now this doesn't matter because all proxy requests use a CONNECT
+        //  tunnel but this likely will not always be the case.
+
+        $uri = $request->getUri();
+        $requestUri = $uri->getPath() ?: '/';
+
+        if ('' !== $query = $uri->getQuery()) {
+            $requestUri .= '?' . $query;
+        }
+
+        $header = $request->getMethod() . ' ' . $requestUri . ' HTTP/' . $protocolVersion . "\r\n";
+
+        try {
+            $header .= Rfc7230::formatHeaders($request->getHeaders());
+        } catch (InvalidHeaderException $e) {
+            throw new HttpException($e->getMessage());
+        }
+
+        return $header . "\r\n";
     }
 }
