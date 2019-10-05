f7ad08557bea8e8bd03e497f46a609a95727192c
Niklas Keller <me@kelunik.com>
2019-10-05 15:18:59 +0200

    Add SetRequestHeaderIfUnset / SetResponseHeaderIfUnset tests

---
+++ test/Interceptor/SetRequestHeaderIfUnsetTest.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Amp\Http\Client\Interceptor;
+
+class SetRequestHeaderIfUnsetTest extends InterceptorTest
+{
+    public function testNetworkInterceptorIfSet(): \Generator
+    {
+        $this->givenNetworkInterceptor(new SetRequestHeader('foo', 'baz'));
+        $this->givenNetworkInterceptor(new SetRequestHeaderIfUnset('foo', 'bar'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestHasHeader('foo', 'baz');
+        $this->thenResponseDoesNotHaveHeader('foo');
+    }
+
+    public function testApplicationInterceptorIfSet(): \Generator
+    {
+        $this->givenApplicationInterceptor(new SetRequestHeader('foo', 'baz'));
+        $this->givenApplicationInterceptor(new SetRequestHeaderIfUnset('foo', 'bar'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestHasHeader('foo', 'baz');
+        $this->thenResponseDoesNotHaveHeader('foo');
+    }
+
+    public function testNetworkInterceptorIfUnset(): \Generator
+    {
+        $this->givenNetworkInterceptor(new SetRequestHeaderIfUnset('foo', 'bar'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestHasHeader('foo', 'bar');
+        $this->thenResponseDoesNotHaveHeader('foo');
+    }
+
+    public function testApplicationInterceptorIfUnset(): \Generator
+    {
+        $this->givenApplicationInterceptor(new SetRequestHeaderIfUnset('foo', 'bar'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestHasHeader('foo', 'bar');
+        $this->thenResponseDoesNotHaveHeader('foo');
+    }
+}
---
+++ test/Interceptor/SetResponseHeaderIfUnsetTest.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Amp\Http\Client\Interceptor;
+
+class SetResponseHeaderIfUnsetTest extends InterceptorTest
+{
+    public function testNetworkInterceptorIfSet(): \Generator
+    {
+        // execution order is reversed
+        $this->givenNetworkInterceptor(new SetResponseHeaderIfUnset('foo', 'bar'));
+        $this->givenNetworkInterceptor(new SetResponseHeader('foo', 'baz'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestDoesNotHaveHeader('foo');
+        $this->thenResponseHasHeader('foo', 'baz');
+    }
+
+    public function testApplicationInterceptorIfSet(): \Generator
+    {
+        // execution order is reversed
+        $this->givenApplicationInterceptor(new SetResponseHeaderIfUnset('foo', 'bar'));
+        $this->givenApplicationInterceptor(new SetResponseHeader('foo', 'baz'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestDoesNotHaveHeader('foo');
+        $this->thenResponseHasHeader('foo', 'baz');
+    }
+
+    public function testNetworkInterceptorIfUnset(): \Generator
+    {
+        $this->givenNetworkInterceptor(new SetResponseHeaderIfUnset('foo', 'bar'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestDoesNotHaveHeader('foo');
+        $this->thenResponseHasHeader('foo', 'bar');
+    }
+
+    public function testApplicationInterceptorIfUnset(): \Generator
+    {
+        $this->givenApplicationInterceptor(new SetResponseHeaderIfUnset('foo', 'bar'));
+
+        yield $this->whenRequestIsExecuted();
+
+        $this->thenRequestDoesNotHaveHeader('foo');
+        $this->thenResponseHasHeader('foo', 'bar');
+    }
+}
