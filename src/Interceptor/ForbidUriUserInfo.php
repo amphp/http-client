ec7c5cfe12f3df07bcd9e6d612df47ce68e7e22a
Niklas Keller <me@kelunik.com>
2019-11-04 23:02:03 +0100

    Improve exception message in ForbidUriUserInfo

--- src/Interceptor/ForbidUriUserInfo.php
+++ src/Interceptor/ForbidUriUserInfo.php
@@ -11,9 +11,13 @@ final class ForbidUriUserInfo extends ModifyRequest
     {
         parent::__construct(static function (Request $request) {
             if ($request->getUri()->getUserInfo() !== '') {
-                throw new HttpException('The user information (username:password) component of URIs has been deprecated '
+                throw new HttpException(
+                    'The user information (username:password) component of URIs has been deprecated '
                     . '(see https://tools.ietf.org/html/rfc3986#section-3.2.1 and https://tools.ietf.org/html/rfc7230#section-2.7.1); '
-                    . 'Instead, set an "Authorization" header containing "Basic " . \\base64_encode("username:password")');
+                    . 'Instead, set an "Authorization" header containing "Basic " . \\base64_encode("username:password"). '
+                    . 'If you used HttpClientBuilder, you can use HttpClientBuilder::allowDeprecatedUriUserInfo() to disable this protection. '
+                    . 'Doing so is strongly discouraged and you need to be aware of any interceptor using UriInterface::__toString(), which might expose the password in headers or logs.'
+                );
             }
         });
     }
