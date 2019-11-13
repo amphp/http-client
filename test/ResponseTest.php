93957f2ad3899c0f168b06ef5d9cecfb08d760e1
Niklas Keller <me@kelunik.com>
2019-11-13 21:24:51 +0100

    Add ResponseTest test cases

--- test/ResponseTest.php
+++ test/ResponseTest.php
@@ -3,16 +3,103 @@
 namespace Amp\Http\Client;
 
 use Amp\ByteStream\InMemoryStream;
-use PHPUnit\Framework\TestCase;
+use Amp\PHPUnit\AsyncTestCase;
 
-class ResponseTest extends TestCase
+class ResponseTest extends AsyncTestCase
 {
+    public function provideInvalidProtocolVersions(): array
+    {
+        return [
+            ["HTTP/1.0"],
+            ["HTTP/1.1"],
+            ["HTTP 1.0"],
+            ["3.0"],
+            ["1.2"],
+        ];
+    }
+
+    /**
+     * @dataProvider provideInvalidProtocolVersions
+     *
+     * @param $invalidVersion
+     */
+    public function testProtocolVersionsAcceptsNoInvalidValues($invalidVersion): void
+    {
+        $this->expectException(\Error::class);
+        $this->expectExceptionMessage("Invalid HTTP protocol version");
+        (new Response($invalidVersion, 200, null, [], new InMemoryStream, new Request('https://localhost/')));
+    }
+
     public function testProtocolVersion(): void
     {
         $response = $this->createResponse();
         $response->setProtocolVersion('2');
 
         $this->assertSame('2', $response->getProtocolVersion());
+    }
+
+    public function testPseudoSetHeader(): void
+    {
+        $this->expectExceptionMessage('Header name cannot be empty or start with a colon (:)');
+
+        (new Response('2', 200, null, [], new InMemoryStream, new Request('https://google.com/')))->setHeader(
+            ':foobar',
+            'foobar'
+        );
+    }
+
+    public function testEmptySetHeader(): void
+    {
+        $this->expectExceptionMessage('Header name cannot be empty or start with a colon (:)');
+
+        (new Response('2', 200, null, [], new InMemoryStream, new Request('https://google.com/')))->setHeader(
+            '',
+            'foobar'
+        );
+    }
+
+    public function testPseudoAddHeader(): void
+    {
+        $this->expectExceptionMessage('Header name cannot be empty or start with a colon (:)');
+
+        (new Response('2', 200, null, [], new InMemoryStream, new Request('https://google.com/')))->addHeader(
+            ':foobar',
+            'foobar'
+        );
+    }
+
+    public function testEmptyAddHeader(): void
+    {
+        $this->expectExceptionMessage('Header name cannot be empty or start with a colon (:)');
+
+        (new Response('2', 200, null, [], new InMemoryStream, new Request('https://google.com/')))->addHeader(
+            '',
+            'foobar'
+        );
+    }
+
+    public function testBody(): \Generator
+    {
+        /** @var Response $response */
+        $response = new Response('2', 200, null, [], new InMemoryStream, new Request('https://google.com/'));
+
+        $response->setBody(null);
+        $this->assertSame('', yield $response->getBody()->buffer());
+
+        $response->setBody("foobar");
+        $this->assertSame('foobar', yield $response->getBody()->buffer());
+
+        $response->setBody($response->getBody());
+        $this->assertSame('foobar', yield $response->getBody()->buffer());
+
+        $response->setBody(new InMemoryStream('foobar2'));
+        $this->assertSame('foobar2', yield $response->getBody()->buffer());
+
+        $response->setBody(143);
+        $this->assertSame('143', yield $response->getBody()->buffer());
+
+        $this->expectException(\TypeError::class);
+        $response->setBody(new \stdClass);
     }
 
     public function testStatus(): void
