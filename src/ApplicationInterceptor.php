9ace59a83691235693ebd7c84a35e2bc30704dad
Niklas Keller <me@kelunik.com>
2019-11-20 01:17:36 +0100

    Add documentation

--- src/ApplicationInterceptor.php
+++ src/ApplicationInterceptor.php
@@ -3,6 +3,7 @@
 namespace Amp\Http\Client;
 
 use Amp\CancellationToken;
+use Amp\Http\Client\Interceptor\DecompressResponse;
 use Amp\Promise;
 
 /**
@@ -17,6 +18,12 @@ interface ApplicationInterceptor
      * the response after the promise returned from `$httpClient->request(...)` resolves.
      *
      * An interceptor might also short-circuit and not delegate to the `$httpClient` at all.
+     *
+     * Any retry or cloned follow-up request must be manually cloned from `$request` to ensure a properly working
+     * interceptor chain, e.g. the {@see DecompressResponse} interceptor only decodes a response if the
+     * `accept-encoding` header isn't set manually. If the request isn't cloned, the first attempt will set the header
+     * and the second attempt will see the header and won't decode the response, because it thinks another interceptor
+     * or the application itself will care about the decoding.
      *
      * @param Request            $request
      * @param CancellationToken  $cancellation
