0eadd3b99945e5aebe340ca41714a886813df941
Daniil Gentili <daniil@daniil.it>
2019-12-17 16:04:43 +0100

    Avoid problems with league URI port set to null if using default port

--- src/Connection/Internal/RequestNormalizer.php
+++ src/Connection/Internal/RequestNormalizer.php
@@ -41,7 +41,7 @@ final class RequestNormalizer
             // Though servers are supposed to be able to handle standard port names on the end of the
             // Host header some fail to do this correctly. As a result, we strip the port from the end
             // if it's a standard 80 or 443
-            if (($scheme === 'http' && $port === 80) || ($scheme === 'https' && $port === 443)) {
+            if (($scheme === 'http' && ($port ?? 80) === 80) || ($scheme === 'https' && ($port ?? 443) === 443)) {
                 $request->setHeader('host', $host);
             } else {
                 $request->setHeader('host', $host . ':' . $port);
