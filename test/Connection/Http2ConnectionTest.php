8c8203e585e14c18542ede7e659bdbf7238e9a5c
Aaron Piotrowski <aaron@trowski.com>
2020-03-10 10:42:52 -0500

    Add HTTP/2 body cancellation tests
    Related to #260.

--- test/Connection/Http2ConnectionTest.php
+++ test/Connection/Http2ConnectionTest.php
@@ -2,6 +2,7 @@
 
 namespace Amp\Http\Client\Connection;
 
+use Amp\CancelledException;
 use Amp\Http\Client\Request;
 use Amp\Http\Client\Response;
 use Amp\Http\Client\Trailers;
@@ -10,7 +11,9 @@ use Amp\Http\Http2\Http2Parser;
 use Amp\Http\Status;
 use Amp\NullCancellationToken;
 use Amp\PHPUnit\AsyncTestCase;
+use Amp\Promise;
 use Amp\Socket;
+use Amp\TimeoutCancellationToken;
 use function Amp\asyncCall;
 use function Amp\delay;
 use function Amp\Http\formatDateHeader;
@@ -166,5 +169,129 @@ class Http2ConnectionTest extends AsyncTestCase
         $trailers = yield $response->getTrailers();
 
         $this->assertSame([], $trailers->getHeaders());
+    }
+
+    public function testCancellingWhileStreamingBody(): \Generator
+    {
+        $hpack = new HPack;
+
+        [$server, $client] = Socket\createPair();
+
+        $connection = new Http2Connection($client);
+
+        $server->write(self::packFrame('', Http2Parser::SETTINGS, 0, 0));
+
+        yield $connection->initialize();
+
+        $request = new Request('http://localhost/');
+
+        /** @var Stream $stream */
+        $stream = yield $connection->getStream($request);
+
+        asyncCall(static function () use ($server, $hpack) {
+            yield delay(100);
+
+            $server->write(self::packFrame($hpack->encode([
+                [":status", Status::OK],
+                ["content-length", "8"],
+                ["date", formatDateHeader()],
+            ]), Http2Parser::HEADERS, Http2Parser::END_HEADERS, 1));
+
+            yield delay(100);
+
+            $server->write(self::packFrame('test', Http2Parser::DATA, Http2Parser::NO_FLAG, 1));
+
+            yield delay(1000);
+
+            $server->write(self::packFrame('test', Http2Parser::DATA, Http2Parser::END_STREAM, 1));
+        });
+
+        /** @var Response $response */
+        $response = yield $stream->request($request, new TimeoutCancellationToken(500));
+
+        $this->assertSame(200, $response->getStatus());
+
+        try {
+            $this->assertSame('test', yield $response->getBody()->buffer());
+            $this->fail("The request body should have been cancelled");
+        } catch (CancelledException $exception) {
+            $buffer = yield $server->read();
+            $expected = self::packFrame(\pack("N", Http2Parser::CANCEL), Http2Parser::RST_STREAM, Http2Parser::NO_FLAG, 1);
+            $this->assertStringEndsWith($expected, $buffer);
+        }
+    }
+
+    public function testCancellingPushPromiseBody(): \Generator
+    {
+        $hpack = new HPack;
+
+        [$server, $client] = Socket\createPair();
+
+        $connection = new Http2Connection($client);
+
+        $server->write(self::packFrame('', Http2Parser::SETTINGS, 0, 0));
+
+        yield $connection->initialize();
+
+        $request = new Request('https://localhost/');
+
+        $request->setPushHandler(function (Request $request, Promise $response) use (&$pushPromise): void {
+            $this->assertSame('/static', $request->getUri()->getPath());
+            $pushPromise = $response;
+        });
+
+        /** @var Stream $stream */
+        $stream = yield $connection->getStream($request);
+
+        asyncCall(static function () use ($server, $hpack) {
+            yield delay(100);
+
+            $server->write(self::packFrame($hpack->encode([
+                [":status", Status::OK],
+                ["content-length", "4"],
+                ["date", formatDateHeader()],
+            ]), Http2Parser::HEADERS, Http2Parser::END_HEADERS, 1));
+
+            $server->write(self::packFrame(\pack("N", 3) . $hpack->encode([
+                [":method", 'GET'],
+                [":authority", 'localhost'],
+                [":scheme", 'https'],
+                [":path", '/static'],
+            ]), Http2Parser::PUSH_PROMISE, Http2Parser::END_HEADERS, 1));
+
+            $server->write(self::packFrame($hpack->encode([
+                [":status", Status::OK],
+                ["content-length", "4"],
+                ["date", formatDateHeader()],
+            ]), Http2Parser::HEADERS, Http2Parser::END_HEADERS, 3));
+
+            yield delay(100);
+
+            $server->write(self::packFrame('test', Http2Parser::DATA, Http2Parser::END_STREAM, 1));
+
+            yield delay(1000);
+
+            $server->write(self::packFrame('test', Http2Parser::DATA, Http2Parser::END_STREAM, 3));
+        });
+
+        /** @var Response $response */
+        $response = yield $stream->request($request, new TimeoutCancellationToken(500));
+
+        $this->assertSame(200, $response->getStatus());
+
+        $this->assertSame('test', yield $response->getBody()->buffer());
+
+        $this->assertInstanceOf(Promise::class, $pushPromise);
+
+        try {
+            $response = yield $pushPromise;
+            \assert($response instanceof Response);
+            yield $response->getBody()->buffer();
+            $this->fail("The push promise body should have been cancelled");
+        } catch (CancelledException $exception) {
+            $buffer = yield $server->read();
+            $expected = self::packFrame(\pack("N", Http2Parser::CANCEL), Http2Parser::RST_STREAM, Http2Parser::NO_FLAG, 3);
+            $this->assertStringEndsWith($expected, $buffer);
+        }
     }
 }
