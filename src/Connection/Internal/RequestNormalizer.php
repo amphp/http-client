092168e709dca99ce27a345bfd48da11f88d8007
Niklas Keller <me@kelunik.com>
2022-03-11 22:42:06 +0100

    Fix TypeError while normalizing TRACE requests

--- src/Connection/Internal/RequestNormalizer.php
+++ src/Connection/Internal/RequestNormalizer.php
@@ -71,7 +71,7 @@ final class RequestNormalizer
         }
 
         // https://tools.ietf.org/html/rfc7231#section-4.3.8
-        $request->setBody(null);
+        $request->setBody('');
 
         // Remove all body and sensitive headers
         $request->setHeaders([
